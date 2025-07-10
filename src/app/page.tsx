d3d3a40cd9bab68f86ed44713eab60659e3ff368
comp-master-byte <demoniac781@gmail.com>
2025-07-10 18:56:49 +0300

    feat(link): share link preview

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,27 @@ import {
   Web3Technologies 
 } from "@/widgets";
 
+export const metadata = {
+  title: 'ViDiPay открывает горизонты!',
+  description: `Global eSim в 150+ странах\n Virtual Card с выгодным курсом\n USDT платежи по QR SBP`,
+  openGraph: {
+    title: 'ViDiPay открывает горизонты!',
+    description: `Global eSim в 150+ странах\n Virtual Card с выгодным курсом\n USDT платежи по QR SBP`,
+    url: 'ViDiPay открывает горизонты!',
+    siteName: 'ViDi',
+    images: [
+      {
+        url: 'https://vidi.vercel.app/url_preview.png',
+        width: 1200,  
+        height: 630,
+        alt: 'ViDiPay Card',
+      },
+    ],
+    locale: 'ru_RU',
+    type: 'website',
+  },
+};
+
 export default function Home() {
   return (
     <div className={styles.app}>

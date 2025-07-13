b4f1594550d355ddbbc117a72e27e21a05f4563a
comp-master-byte <demoniac781@gmail.com>
2025-07-13 15:27:18 +0300

    fix(marquee): added lib for marquee component

--- src/shared/components/Marquee/Marquee.tsx
+++ src/shared/components/Marquee/Marquee.tsx
@@ -1,6 +1,7 @@
 import { memo } from "react";
 import styles from './Marquee.module.css';
 import { IconCard } from "../IconCard";
+import FastMarquee from "react-fast-marquee";
 
 type MarqueeItem = {
   id: number | string;
@@ -16,9 +17,24 @@ export const Marquee = memo(function Marquee({
   list
 }: MarqueeProps) {
   return (
-    <div className={styles.marquee}>
-      <div className={styles.marqueeContent}>
-        {[...list, ...list, ...list].map((item, index) => (
+    <>
+      {/* <div className={styles.marquee}>
+        <div className={styles.marqueeContent}>
+          {[...list, ...list, ...list].map((item, index) => (
+            <IconCard
+              key={`${item.id}-${index}`}
+              icon={item.icon}
+              bgColor={item.bgColor}
+              fullIcon={item.fullIcon}
+              className={styles.marqueeItem}
+            />
+          ))}
+        </div>
+      </div>
+     */}
+
+      <FastMarquee>
+        {list.map((item, index) => (
           <IconCard
             key={`${item.id}-${index}`}
             icon={item.icon}
@@ -27,7 +43,7 @@ export const Marquee = memo(function Marquee({
             className={styles.marqueeItem}
           />
         ))}
-      </div>
-    </div>
+      </FastMarquee>
+    </>
   )
 })

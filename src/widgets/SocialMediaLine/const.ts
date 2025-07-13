b5295facf2d394b2a7220a3d2f8142c73681d242
comp-master-byte <demoniac781@gmail.com>
2025-07-13 17:43:52 +0300

    fix(marquee): changed social media icons

--- src/widgets/SocialMediaLine/const.ts
+++ src/widgets/SocialMediaLine/const.ts
@@ -1,21 +1,27 @@
-import googlePlay from '@/shared/assets/icons/social_media_logos/google_play.svg';
-import youtube from '@/shared/assets/icons/social_media_logos/youtube.svg';
-import airbnb from '@/shared/assets/icons/social_media_logos/airbnb.svg';
-import zoom from '@/shared/assets/icons/social_media_logos/zoom.svg';
-import netflix from '@/shared/assets/icons/social_media_logos/netflix.svg';
-import canva from '@/shared/assets/icons/social_media_logos/canva.svg';
-import appStore from '@/shared/assets/icons/social_media_logos/appStore.svg';
-import miro from '@/shared/assets/icons/social_media_logos/miro.svg';
-import booking from '@/shared/assets/icons/social_media_logos/booking.svg';
+import yango from '@/shared/assets/icons/social_media_logos/full_width/yango.svg';
+import googlePlay from '@/shared/assets/icons/social_media_logos/full_width/google_play.svg';
+import youtube from '@/shared/assets/icons/social_media_logos/full_width/youtube.svg';
+import airbnb from '@/shared/assets/icons/social_media_logos/full_width/airbnb.svg';
+import zoom from '@/shared/assets/icons/social_media_logos/full_width/zoom.svg';
+import netflix from '@/shared/assets/icons/social_media_logos/full_width/netflix.svg';
+import canva from '@/shared/assets/icons/social_media_logos/full_width/canva.svg';
+import appStore from '@/shared/assets/icons/social_media_logos/full_width/appStore.svg';
+import miro from '@/shared/assets/icons/social_media_logos/full_width/miro.svg';
+import iMove from '@/shared/assets/icons/social_media_logos/full_width/iMove.svg';
+import spotify from '@/shared/assets/icons/social_media_logos/full_width/spotify.svg';
+import booking from '@/shared/assets/icons/social_media_logos/full_width/booking.svg';
 
 export const SOCIAL_MEDIA = [
-  {id: 1, icon: googlePlay, bgColor: '#ECECEC'},
-  {id: 2, icon: youtube, bgColor: '#ECECEC'},
-  {id: 3, icon: airbnb, bgColor: '#ECECEC'},
-  {id: 4, icon: zoom, bgColor: '#2B98F0'},
-  {id: 5, icon: netflix, bgColor: '#424242'},
+  {id: 0, icon: yango, fullIcon: true},
+  {id: 1, icon: googlePlay, fullIcon: true},
+  {id: 2, icon: youtube, fullIcon: true},
+  {id: 3, icon: airbnb, fullIcon: true},
+  {id: 4, icon: zoom, fullIcon: true},
+  {id: 5, icon: netflix, fullIcon: true},
   {id: 6, icon: canva, fullIcon: true},
   {id: 7, icon: appStore, fullIcon: true},
-  {id: 8, icon: miro, bgColor: '#FECF43'},
-  {id: 9, icon: booking, bgColor: '#103D7A'},
+  {id: 8, icon: miro, fullIcon: true},
+  {id: 9, icon: iMove, fullIcon: true},
+  {id: 9, icon: spotify, fullIcon: true},
+  {id: 10, icon: booking, fullIcon: true},
 ]

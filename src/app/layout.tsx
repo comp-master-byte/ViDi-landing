b3e57fdf2bc0586ac914c83c8f9974495dc14665
comp-master-byte <demoniac781@gmail.com>
2025-07-15 01:50:14 +0300

    feat(metrics): added yandex metrics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './styles/global.css';
 import { Manrope } from 'next/font/google';
+import Script from 'next/script';
 
 const manrope = Manrope({
   subsets: ['latin', 'cyrillic'],
@@ -14,6 +15,41 @@ export default function RootLayout({
   return (
     <html lang="en" className={manrope.className}>
       <body>
+        <Script
+          strategy="afterInteractive"
+          src="https://mc.yandex.ru/metrika/tag.js"
+        />
+        <Script id="yandex-metrika-init" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) { return; }
+              }
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],
+              k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(103332335, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true,
+              webvisor:true
+            });
+          `}
+        </Script>
+
+        {/* NoScript Fallback */}
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/103332335"
+              style={{ position: 'absolute', left: '-9999px' }}
+              alt=""
+            />
+          </div>
+        </noscript>
         {children}
       </body>
     </html>
